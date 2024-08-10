94fa044986f5355d97d21996a6af5a1ad3fcccda
username <wecp-dummy@gmail.com>
2024-08-10 03:57:56 +0000

    wecp initial commit

---
+++ server/src/main/java/com/wecp/eventmanagementsystem/service/UserService.java
@@ -0,0 +1,28 @@
+package com.wecp.eventmanagementsystem.service;
+
+import com.wecp.eventmanagementsystem.entity.User;
+import com.wecp.eventmanagementsystem.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public Long registerUser(User user){
+        return userRepository.save(user).getUserID();
+    }
+
+    // public User loginUser(LoginRequest loginRequest){
+        
+    // }
+
+}
